b3de635215323c5d8672a0915a5c9dcb9e54da61
soheee <soan32@hanmail.net>
2023-02-22 11:36:27 +0900

    urlchange

--- prepare/front/components/post/PostImages.js
+++ prepare/front/components/post/PostImages.js
@@ -1,8 +1,9 @@
-import React, { useCallback, useState } from 'react';
-import PropTypes from 'prop-types';
-import { PlusOutlined } from '@ant-design/icons';
+import React, { useCallback, useState } from "react";
+import PropTypes from "prop-types";
+import { PlusOutlined } from "@ant-design/icons";
 
-import ImagesZoom from './ImagesZoom';
+import ImagesZoom from "./ImagesZoom";
+import { backUrl } from "../../config/config";
 
 const PostImages = ({ images }) => {
   const [showImagesZoom, setShowImagesZoom] = useState(false);
@@ -17,7 +18,12 @@ const PostImages = ({ images }) => {
   if (images.length === 1) {
     return (
       <>
-        <img role="presentation" src={`http://localhost:3065/${images[0].src}`} alt={images[0].src} onClick={onZoom} />
+        <img
+          role="presentation"
+          src={`${backUrl}/${images[0].src}`}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
         {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
       </>
     );
@@ -25,8 +31,20 @@ const PostImages = ({ images }) => {
   if (images.length === 2) {
     return (
       <>
-        <img role="presentation" style={{ width: '50%', display: 'inline-block' }} src={`http://localhost:3065/${images[0].src}`} alt={images[0].src} onClick={onZoom} />
-        <img role="presentation" style={{ width: '50%', display: 'inline-block' }} src={`http://localhost:3065/${images[1].src}`} alt={images[1].src} onClick={onZoom} />
+        <img
+          role="presentation"
+          style={{ width: "50%", display: "inline-block" }}
+          src={`http://localhost:3065/${images[0].src}`}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+        <img
+          role="presentation"
+          style={{ width: "50%", display: "inline-block" }}
+          src={`${backUrl}/${images[1].src}`}
+          alt={images[1].src}
+          onClick={onZoom}
+        />
         {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
       </>
     );
@@ -34,10 +52,21 @@ const PostImages = ({ images }) => {
   return (
     <>
       <div>
-        <img role="presentation" style={{ width: '50%' }} src={`http://localhost:3065/${images[0].src}`} alt={images[0].src} onClick={onZoom} />
+        <img
+          role="presentation"
+          style={{ width: "50%" }}
+          src={`http://localhost:3065/${images[0].src}`}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
         <div
           role="presentation"
-          style={{ display: 'inline-block', width: '50%', textAlign: 'center', verticalAlign: 'middle' }}
+          style={{
+            display: "inline-block",
+            width: "50%",
+            textAlign: "center",
+            verticalAlign: "middle",
+          }}
           onClick={onZoom}
         >
           <PlusOutlined />
